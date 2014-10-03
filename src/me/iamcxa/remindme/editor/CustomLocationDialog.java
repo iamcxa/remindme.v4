8db3ddacde2fa26a2b8588f89fcf15c6f280e081
iamcxa <iamcxa@gmail.com>
2014-10-03 14:31:16 +0800

    merge with location

--- src/me/iamcxa/remindme/editor/CustomLocationDialog.java
+++ src/me/iamcxa/remindme/editor/CustomLocationDialog.java
@@ -2,12 +2,16 @@ package me.iamcxa.remindme.editor;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.GoogleMap.OnInfoWindowClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMarkerDragListener;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
@@ -39,7 +43,9 @@ import android.view.WindowManager;
 import android.view.ContextMenu.ContextMenuInfo;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TabHost;
@@ -52,7 +58,10 @@ import android.widget.Toast;
  * Tab 1 will be a list view. Tab 2 will be a list view.
  * 
  */
-public class CustomLocationDialog extends DialogFragment
+public class CustomLocationDialog extends DialogFragment implements
+	OnMarkerClickListener,
+	OnInfoWindowClickListener,
+	OnMarkerDragListener
 {
 	private static  CommonEditorVar mEditorVar=CommonEditorVar.GetInstance();
 
@@ -63,12 +72,18 @@ public class CustomLocationDialog extends DialogFragment
 	 */
 	private static GoogleMap map;
 	private static MapView mapView;
-	private SupportMapFragment fragment;
-
 	private View mContentView;
+	private static TextView PlaceName;
+	private static EditText SearchText;
+	private static Button Search;
+	
+	private String locationName;
+	private double Lat;
+	private double Lon;
 	
 	public CustomLocationDialog newInstance() {
 		return new CustomLocationDialog();
+		
     }
 	
 	 @Override
@@ -79,9 +94,12 @@ public class CustomLocationDialog extends DialogFragment
 	    @Override
 	    public View onCreateView(LayoutInflater inflater, ViewGroup container,
 	            Bundle savedInstanceState) {
-	    	
+	    	getDialog().requestWindowFeature(Window.FEATURE_NO_TITLE);
 	    	mContentView= inflater.inflate(R.layout.activity_task_editor_tab_location, container, false);
-	    	 
+	    	PlaceName =(TextView)mContentView.findViewById(R.id.PlaceName);
+	    	SearchText = (EditText) mContentView.findViewById(R.id.SearchText);
+	    	Search = (Button) mContentView.findViewById(R.id.Search);
+			Search.setOnClickListener(SearchPlace);
 	    	 MapsInitializer.initialize(getActivity());
 	    	 
 	    	 switch (GooglePlayServicesUtil.isGooglePlayServicesAvailable(getActivity()) )
@@ -90,16 +108,11 @@ public class CustomLocationDialog extends DialogFragment
 //	                 Toast.makeText(getActivity(), "SUCCESS", Toast.LENGTH_SHORT).show();
 	                 mapView = (MapView) mContentView.findViewById(R.id.map);
 	                 mapView.onCreate(savedInstanceState);
-//	                 setUpMapIfNeeded();
 	                 // Gets to GoogleMap from the MapView and does initialization stuff
 	                 if(mapView!=null)
 	                 {
+	                	 mapView.onResume();
 	                	 setUpMapIfNeeded();
-//	                     map = mapView.getMap();
-//	                     map.getUiSettings().setMyLocationButtonEnabled(false);
-//	                     map.setMyLocationEnabled(true);
-//	                     CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(43.1, -87.9), 10);
-//	                     map.animateCamera(cameraUpdate);
 	                 }
 	                 break;
 	             case ConnectionResult.SERVICE_MISSING: 
@@ -116,8 +129,14 @@ public class CustomLocationDialog extends DialogFragment
 
 	        return mContentView;
 	    }
-	    
-	    
+	     
+	@Override
+		public void onActivityCreated(Bundle savedInstanceState) {
+			// TODO Auto-generated method stub
+			super.onActivityCreated(savedInstanceState);
+//			PlaceName = (TextView) mContentView.findViewById(R.id.PlaceName);
+		}
+
 	private void setUpMapIfNeeded()
 	{
 //	    if(map == null)
@@ -132,6 +151,7 @@ public class CustomLocationDialog extends DialogFragment
 	        	map.getUiSettings().setZoomControlsEnabled(false);
 	        	map.setMyLocationEnabled(true);
 	        	map.setOnCameraChangeListener(listener);
+	        	map.setOnMapClickListener(mapclickListener);
 //	        	map.setOnMarkerClickListener(MarkerClickListener);
 	    		LatLng nowLoacation;
 //	    		if (gpsManager.LastLocation() != null) {
@@ -155,15 +175,64 @@ public class CustomLocationDialog extends DialogFragment
 		@Override
 		public void onCameraChange(CameraPosition position) {
 			// TODO Auto-generated method stub
-			map.clear();
-			LatLng now = new LatLng(position.target.latitude, position.target.longitude);
-			map.addMarker(new MarkerOptions()
-            .title("目的地")
-            .position(now));
+//			map.clear();
+//			LatLng now = new LatLng(position.target.latitude, position.target.longitude);
+//			map.addMarker(new MarkerOptions()
+//            .title("目的地")
+//            .position(now));
 		}
 	};
 	
+	private GoogleMap.OnMapClickListener mapclickListener = new GoogleMap.OnMapClickListener() {
+		
+		@Override
+		public void onMapClick(LatLng point) {
+			// TODO Auto-generated method stub
+			map.clear();
+			LatLng now = new LatLng(point.latitude, point.longitude);
+			map.addMarker(new MarkerOptions()
+            .title("目的地")
+            .position(now)
+            .draggable(true));
+		}
+	};
 	
+	private Button.OnClickListener SearchPlace = new Button.OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			if(v.getId()==R.id.Search)
+				SearchPlace();
+		}
+		
+	};
+	private void SearchPlace() {
+		if (!SearchText.getText().toString().equals("")) {
+			GeocodingAPI LoacationAddress2 = null;
+			LatLng SearchLocation = null;
+			LoacationAddress2 = new GeocodingAPI(getActivity(),
+					SearchText.getText().toString());
+			// textView2.setText("");
+			// locationName=LoacationAddress2.GeocodingApiAddressGet();
+			// textView2.setText(textView2.getText()+"\n"+Address);
+			SearchLocation = LoacationAddress2.GeocodingApiLatLngGet();
+			Lat=SearchLocation.latitude;
+			Lon=SearchLocation.longitude;
+			// textView2.setText(textView2.getText()+"\n"+SearchLocation);
+			locationName=LoacationAddress2.GeocodingApiAddressGet();
+			PlaceName.setText(locationName);
+			if (SearchLocation != null) {
+				map.animateCamera((CameraUpdateFactory.newLatLngZoom(
+						SearchLocation, map.getMaxZoomLevel() - 4)));
+				map.addMarker(new MarkerOptions().title("搜尋的位置")
+						.snippet(locationName).position(SearchLocation));
+			} else {
+				Toast.makeText(getActivity(), "查無地點哦,換個詞試試看",
+						Toast.LENGTH_SHORT).show();
+			}
+		}
+	}
 	/**
 	 * This is called when a long press occurs on our listView02 items.
 	 */
@@ -190,5 +259,35 @@ public class CustomLocationDialog extends DialogFragment
 		}
 
 		return true;  
+	}
+
+	@Override
+	public void onMarkerDrag(Marker marker) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onMarkerDragEnd(Marker marker) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onMarkerDragStart(Marker marker) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onInfoWindowClick(Marker marker) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean onMarkerClick(Marker marker) {
+		// TODO Auto-generated method stub
+		return false;
 	}  
 }
